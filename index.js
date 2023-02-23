c7c8108d84bf79b7728bbad3fa3ebe70fa30d5c1
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-23 19:18:39 +0600

    this module is not done

---
+++ index.js
@@ -0,0 +1,16 @@
+function min(nums) {
+    return Math.min(nums)
+}
+// console.log(min([1, 3, 2]))
+
+
+const cube = x => x * x * x;
+// console.log(cube(2))
+
+const [a, b] = [1, 2, 3, 4, 45, 5];
+console.log(a + b);
+
+
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
